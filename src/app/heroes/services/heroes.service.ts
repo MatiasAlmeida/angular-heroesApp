7d298be33882013a6343a47fafe279676353a9fe
Matias Almeida <matiasleonelalmeida97@gmail.com>
2023-10-14 22:11:28 -0300

    added routing per hero with mat design

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
@@ -13,5 +13,12 @@ export class HeroesService {
 
   public getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+  public getHeroById( id: string ): Observable<Hero | undefined> {
+    return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        catchError( () => of(undefined) )
+      );
   }
 }
